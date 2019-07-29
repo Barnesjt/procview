f806c22b44b2cfb42ef0a09d4ef15570832015a5
Jack Barnes <barnesjt05@gmail.com>
2019-07-29 13:28:33 -0700

    xp command added to view executable memory pages

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use sysinfo::{System, SystemExt, ProcessExt, Pid};
 use std::io::{self, BufRead, Write};
 use std::str::FromStr;
 use std::collections::{BTreeMap, BTreeSet};
-use std::path::Path;
-use std::ffi::OsStr;
-use std::result::Result;
+//use std::path::Path;
+//use std::ffi::OsStr;
+//use std::result::Result;
 
 fn print_help() {
     writeln!(&mut io::stdout(), "               ==       procview       ==               ");
@@ -21,7 +21,7 @@ fn print_help() {
     writeln!(&mut io::stdout(), "              ps : View All Processes                   ");
     writeln!(&mut io::stdout(), "       pst <pid> : View Process Threads                 ");
     writeln!(&mut io::stdout(), "        lm <pid> : View Loaded Modules Within Process   ");
-    writeln!(&mut io::stdout(), "        ep <pid> : View Executable Pages Within Process ");
+    writeln!(&mut io::stdout(), "        xp <pid> : View Executable Pages Within Process ");
     writeln!(&mut io::stdout(), " mem <pid> <loc> : View Process Memory At Location      ");
 }
 
@@ -64,26 +64,18 @@ fn parse_input(input: &str, sys: &mut System) -> bool {
             if tmp.len() != 2 {
                 writeln!(&mut io::stdout(), "lm command expects a pid argument");
             } else if let Ok(pid) = Pid::from_str(tmp[1]) {
-                let all_procs = procfs::all_processes();
-                for process in &all_procs {
-                    if process.pid() == pid {
-                        writeln!(&mut io::stdout(), "Loaded Modules For PID: {}", pid);
-                        match process.maps() {
-                            Ok(map) => {
-                                let mut modules = BTreeSet::new();
-                                for elem in &map {
-                                    match &elem.pathname {
-                                        procfs::MMapPath::Path(p) => modules.insert(p.to_str().unwrap().to_string()),
-                                        _e => false
-                                    };
-                                }
-                                for module in &modules {
-                                    writeln!(&mut io::stdout(), "|---- {}", module);
-                                }
-                            },
-                            _e => {}
-                        };
-                    }
+                for module in find_loaded_modules(pid) {
+                    writeln!(&mut io::stdout(), "|---- {}", module);
+                }
+            }
+        }
+        e if e.starts_with("xp ") => {
+            let tmp : Vec<&str> = e.split(' ').collect();
+            if tmp.len() != 2 {
+                writeln!(&mut io::stdout(), "xp command expects a pid argument");
+            } else if let Ok(pid) = Pid::from_str(tmp[1]) {
+                for ((add1, add2), name) in find_exec_pages(pid).iter() {
+                    writeln!(&mut io::stdout(), "|---- {:X?} - {:X?} :\t{}", add1, add2, name);
                 }
             }
         }
@@ -95,9 +87,56 @@ fn parse_input(input: &str, sys: &mut System) -> bool {
     false
 }
 
-/* fn find_loaded_modules(pid: pid_t) -> BTreeSet {
+fn find_loaded_modules(pid: i32) -> BTreeSet<String> {
+    let mut modules = BTreeSet::new();
+    writeln!(&mut io::stdout(), "Loaded Modules For PID: {}", pid);
+    for process in procfs::all_processes() {
+        if process.pid() == pid {
+            match process.maps() {
+              Ok(map) => {
+                  for elem in &map {
+                       match &elem.pathname {
+                          procfs::MMapPath::Path(p) => modules.insert(p.to_str().unwrap().to_string()),
+                           _e => false
+                        };
+                 }
+              },
+            _e => {}
+          };
+        }
+    }
+    modules
+}
 
-} */
+fn find_exec_pages(pid: i32) -> BTreeMap<(u64, u64), String> {
+    let mut exec_pages = BTreeMap::new();
+    writeln!(&mut io::stdout(), "Executable Pages For PID: {}", pid);
+    for process in procfs::all_processes() {
+        if process.pid() == pid {
+            match process.maps() {
+              Ok(map) => {
+                  for elem in &map {
+                      if elem.perms.contains("x") {
+                        match &elem.pathname {
+                            procfs::MMapPath::Path(p) => exec_pages.insert(elem.address, p.to_str().unwrap().to_string()),
+                            procfs::MMapPath::Heap => exec_pages.insert(elem.address, String::from("Heap")),
+                            procfs::MMapPath::Stack => exec_pages.insert(elem.address, String::from("Stack")),
+                            procfs::MMapPath::TStack(tid) => exec_pages.insert(elem.address, format!("Thread Stack. TID: {}", tid.to_string())),
+                            procfs::MMapPath::Vdso => exec_pages.insert(elem.address, String::from("Virtual Dynamically Linked Shared Object")),
+                            procfs::MMapPath::Vvar => exec_pages.insert(elem.address, String::from("Shared kernel variables")),
+                            procfs::MMapPath::Vsyscall => exec_pages.insert(elem.address, String::from("Virtual syscalls")),
+                            procfs::MMapPath::Anonymous => exec_pages.insert(elem.address, String::from("Anonymous/Private")),
+                            procfs::MMapPath::Other(p) => exec_pages.insert(elem.address, format!("Other: {}", p))
+                        }; 
+                      }
+                 }
+              },
+            _e => {}
+          };
+        }
+    }
+    exec_pages
+}
 
 fn main() {
     let mut t = System::new();
